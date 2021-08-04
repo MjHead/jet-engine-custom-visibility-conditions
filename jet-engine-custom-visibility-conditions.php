f30d1b847bc2551d9d56a227a06a9d3977d549b9
Adel Tahri <adel4tahri@gmail.com>
2021-08-04 12:25:49 +0100

    added post-date.php file

--- jet-engine-custom-visibility-conditions.php
+++ jet-engine-custom-visibility-conditions.php
@@ -25,18 +25,23 @@ function jet_engine_cvc() {
 	define( 'JET_ECVC_PLUGIN_BASE', plugin_basename( JET_ECVC__FILE__ ) );
 	define( 'JET_ECVC_PATH', plugin_dir_path( JET_ECVC__FILE__ ) );
 
-	add_action( 'jet-engine/modules/dynamic-visibility/conditions/register', function( $conditions_manager ) {
+	add_action(
+		'jet-engine/modules/dynamic-visibility/conditions/register',
+		function( $conditions_manager ) {
 
-		require JET_ECVC_PATH . 'post-status.php';
-		require JET_ECVC_PATH . 'post-by-user.php';
-		require JET_ECVC_PATH . 'user-meta.php';
-		require JET_ECVC_PATH . 'user-meta-is-post-meta.php';
+			require JET_ECVC_PATH . 'post-date.php';
+			require JET_ECVC_PATH . 'post-status.php';
+			require JET_ECVC_PATH . 'post-by-user.php';
+			require JET_ECVC_PATH . 'user-meta.php';
+			require JET_ECVC_PATH . 'user-meta-is-post-meta.php';
 
-		$conditions_manager->register_condition( new Jet_Engine_CVC\Post_Status() );
-		$conditions_manager->register_condition( new Jet_Engine_CVC\Post_By_User() );
-		$conditions_manager->register_condition( new Jet_Engine_CVC\User_Meta_Is() );
-		$conditions_manager->register_condition( new Jet_Engine_CVC\User_Meta_Is_Post_Meta() );
+			$conditions_manager->register_condition( new Jet_Engine_CVC\Post_Date() );
+			$conditions_manager->register_condition( new Jet_Engine_CVC\Post_Status() );
+			$conditions_manager->register_condition( new Jet_Engine_CVC\Post_By_User() );
+			$conditions_manager->register_condition( new Jet_Engine_CVC\User_Meta_Is() );
+			$conditions_manager->register_condition( new Jet_Engine_CVC\User_Meta_Is_Post_Meta() );
 
-	} );
+		}
+	);
 
 }
